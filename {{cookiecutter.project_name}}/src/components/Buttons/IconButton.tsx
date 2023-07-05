3b9061e7b1f2a4afd88dc01cd52342baee32a361
RaulSCoelho <84609153+RaulSCoelho@users.noreply.github.com>
2023-07-05 17:53:54 -0300

    Merge branch 'main' into test/mongodb

--- {{cookiecutter.project_name}}/src/components/Buttons/IconButton.tsx
+++ {{cookiecutter.project_name}}/src/components/Buttons/IconButton.tsx
@@ -1,15 +1,19 @@
 import { ReactNode } from 'react'
 
+import classnames from 'classnames'
+
 import { ButtonBase, ButtonBaseProps } from './ButtonBase'
 
 export interface IconButtonProps extends Omit<ButtonBaseProps, 'children'> {}
 
 export function IconButton({ icon, className, ...props }: IconButtonProps & { icon: ReactNode }) {
+  const classes = classnames(
+    'flex h-8 w-8 items-center justify-center rounded-[50%] bg-gray-500 bg-opacity-10',
+    className
+  )
+
   return (
-    <ButtonBase
-      className={`flex h-8 w-8 items-center justify-center rounded-[50%] bg-gray-500 bg-opacity-10 ${className}`}
-      {...props}
-    >
+    <ButtonBase className={classes} {...props}>
       {icon}
     </ButtonBase>
   )
--- {{cookiecutter.project_name}}/src/components/Switchers/DayNightSwitch/SunMoon.tsx
+++ {{cookiecutter.project_name}}/src/components/Switchers/DayNightSwitch/SunMoon.tsx
@@ -43,7 +43,7 @@ export function SunMoon({ side, size, onClick }: Props) {
   }, [size])
 
   function handleClick() {
-    onClick && onClick(positions.moon[side] === '0' ? 'day' : 'night')
+    onClick?.(positions.moon[side] === '0' ? 'day' : 'night')
   }
 
   return (
